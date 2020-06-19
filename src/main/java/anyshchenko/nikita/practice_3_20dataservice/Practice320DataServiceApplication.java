1ddd959a99ec00df342a2e22d7a01c8b25081d4f
Mykyta Anishchenko <mykyta.anyshchenko@netcracker.com>
2020-06-19 20:25:23 +0300

    Start

---
+++ src/main/java/anyshchenko/nikita/practice_3_20dataservice/Practice320DataServiceApplication.java
@@ -0,0 +1,13 @@
+package anyshchenko.nikita.practice_3_20dataservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practice320DataServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Practice320DataServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/anyshchenko/nikita/practice_3_20dataservice/Practice320DataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package anyshchenko.nikita.practice_3_20dataservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practice320DataServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
